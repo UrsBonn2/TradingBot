b286bd2ea4c93830b656d9eceed71fabec579e5a
UrsBonn2 <Juergen.Frensch@yahoo.de>
2025-07-09 01:26:49 -0700

    Update app.py
    Update app.py :  Logging explizit aktivieren

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request
+import logging
 
 app = Flask(__name__)
+
+# Logging aktivieren
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'POST':
-        data = request.json
-        print("Received Alert:", data)
+        data = request.get_json(force=True, silent=True)
+        app.logger.info(f"📩 Received POST: {data}")
         return '', 200
     else:
         return 'Webhook server is online', 200
