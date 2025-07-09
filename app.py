eaa24708beb836741db30a53ebdc42824f8363bf
UrsBonn2 <Juergen.Frensch@yahoo.de>
2025-07-09 01:07:46 -0700

    Update app.py
    Updated app.py with GET+POST route

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def webhook():
-    data = request.json
-    print("Received Alert:", data)
-    return '', 200
+    if request.method == 'POST':
+        data = request.json
+        print("Received Alert:", data)
+        return '', 200
+    else:
+        return 'Webhook server is online', 200
 
 if __name__ == '__main__':
     app.run()
